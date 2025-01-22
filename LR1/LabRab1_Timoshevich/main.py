4afd132152aa0f4313fc9d4f66c0eed1e78d45e5
Karina Timoshevich <karina03tima@gmail.com>
2025-01-22 17:47:43 +0300

    LabRab1 completed

--- LR1/LabRab1_Timoshevich/main.py
+++ LR1/LabRab1_Timoshevich/main.py
@@ -1,23 +1,27 @@
 import numpy as np
 
 
-def is_invertible_and_find_inverse(A, A_inv, x, i):
-
+def solution(A, A_inv, x, i):
     n = len(A)
-    l = np.dot(A_inv, x)
+    if i < 0 or i >= n:
+        print(f"Ошибка: индекс i = {i} выходит за пределы допустимого диапазона (0-{n - 1}).")
+        return False, None
+
+    l = np.dot(A_inv, x)  # шаг 1
 
     if l[i] == 0:
         return False, None
 
     l_e = l.copy()
-    l_e[i] = -1
-    l_b = -l_e / l[i]
+    l_e[i] = -1  # шаг 2
+    l_b = -l_e / l[i]  # шаг 3
 
     Q = np.eye(n)
-    Q[:, i] = l_b
-    A_new_inv = np.dot(Q, A_inv)
+    Q[:, i] = l_b  # шаг 4
+    A_new_inv = np.dot(Q, A_inv)  # шаг 5
 
     return True, A_new_inv
+
 
 A = np.array([[1, 0, 1],
               [0, 0, 0],
@@ -28,13 +32,13 @@ A_inv = np.array([[1, 1, 0],
                   [0, 0, 1]])
 
 x = np.array([1, 0, 1])
-i = 2
+i = 2 
 
-invertible, A_new_inv = is_invertible_and_find_inverse(A, A_inv, x, i)
+invertible, A_new_inv = solution(A, A_inv, x, i)
 
 if invertible:
     print("Матрица обратима.")
     print("Обратная матрица:")
     print(A_new_inv)
 else:
-    print("Матрица необратима.")
+    print("Матрица необратима или произошла ошибка.")
